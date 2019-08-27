c8645a92772c69998556f97f51658794175e6093
Muhammad Natsir <mnatsirm53@gmail.com>
2019-08-27 17:40:52 +0700

    Create aboveaverage.py

---
+++ OpenKattis/Above Average/aboveaverage.py
@@ -0,0 +1,23 @@
+number_test_case = int(input())
+rate_above = []
+
+def above_average(values):
+    mean = sum(values) / number_student
+    sucess = 0
+    for num in values:
+        if num > mean:
+            sucess += 1
+    
+    return sucess
+
+for num in range(number_test_case):
+    number = input().split()
+    number = [int(num) for num in number]
+    
+    number_student = number[0]
+    values = number[1:]
+
+    rate_above.append(above_average(values) / number_student * 100)
+    
+for rate in rate_above:
+    print(f"{format(rate, '.3f')}%")
